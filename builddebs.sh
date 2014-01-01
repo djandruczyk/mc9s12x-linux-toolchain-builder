e3713fe640f61046d221b9abe856adae70c58ac2
David Andruczyk <dandruczyk@gmail.com>
2014-01-01 16:00:26 -0500

    converted mc9s12x-toolchain into a more official style metapkg, force upload tarballs to be bz2s for space savings, adjusted watches to refer to the bz2 tarballs more sane variable naming in build script, fixed missing gcc download section

--- builddebs.sh
+++ builddebs.sh
@@ -4,11 +4,12 @@
 #  Makes Debs for recent ubuntu and debian
 #
 
-WORKDIR=`pwd`
-TMPDIR="${WORKDIR}/tmp"
+WORK_DIR=`pwd`
+TMP_DIR="${WORK_DIR}/tmp"
 BINUTILS_GIT="http://git.libreems.org/libreems-suite/s12x-binutils.git"
 GCC_GIT="http://git.libreems.org/libreems-suite/s12x-gcc.git"
 GCC_BRANCH="tmp-for-dave"
+GCC_URI="http://www.netgull.com/gcc/releases/gcc-3.3.6/"
 GCC_TAR="gcc-3.3.6.tar.bz2"
 GCC_PKG="gcc-mc9s12x"
 GCC_DIR="gcc-3.3.6"
@@ -17,9 +18,9 @@ NEWLIB_DIR="newlib-mc9s12x"
 BINUTILS_URI="http://ftp.gnu.org/gnu/binutils/"
 BINUTILS_TAR="binutils-2.24.tar.bz2"
 METAPKG_DIR="mc9s12x-toolchain"
-OUTDIR="${WORKDIR}"/Output
+OUT_DIR="${WORK_DIR}"/Output
 OTHERMIRROR=/var/cache/pbuilder/repo
-BUILDDIR="${WORKDIR}"/build
+BUILD_DIR="${WORK_DIR}"/build
 BINUTILS_PKGS="binutils-mc9s12x binutils-xgate"
 CPUS=`cat /proc/cpuinfo |grep -c ^processor:`
 if test `uname -m` = "x86_64" ; then
@@ -44,11 +45,11 @@ echo "Building for \"${DEB_RELEASES}\""
 #
 function build_debs {
 for dist in `echo "${DEB_RELEASES}"` ; do
-	perl -pi -e "s/\)/~${dist}\)/g if 1 .. 1" debian/changelog
-	perl -pi -e "s/\ [a-z]+;/\ ${dist};/g if 1 .. 1" debian/changelog
+#	perl -pi -e "s/\)/~${dist}\)/g if 1 .. 1" debian/changelog
+#	perl -pi -e "s/\ [a-z]+;/\ ${dist};/g if 1 .. 1" debian/changelog
 	for arch in `echo ${ARCHS}` ; do
 		echo "Building for Distro $dist Arch $arch"
-		DESTDIR="${OUTDIR}"/"${dist}"/"${arch}"
+		DESTDIR="${OUT_DIR}"/"${dist}"/"${arch}"
 		if [ ! -d "${DESTDIR}" ] ; then
 			mkdir -p "${DESTDIR}"
 		fi
@@ -65,22 +66,22 @@ for dist in `echo "${DEB_RELEASES}"` ; do
 		fi
 		find ${OTHERMIRROR} -type f -exec rm -f {} \;
 	done
-	perl -pi -e "s/\ ${dist};/\ unstable;/g if 1 .. 1" debian/changelog
-	perl -pi -e "s/~${dist}\)/\)/g if 1 .. 1" debian/changelog
+#	perl -pi -e "s/\ ${dist};/\ unstable;/g if 1 .. 1" debian/changelog
+#	perl -pi -e "s/~${dist}\)/\)/g if 1 .. 1" debian/changelog
 done
 }
 
 function setup {
-mkdir -p "${BUILDDIR}"
+mkdir -p "${BUILD_DIR}"
 }
 
 # Need to make multiple versions of binutils, 
 # one for S12x, and one for Xgate
 function build_binutils {
-if [ ! -f ${TMPDIR}/${BINUTILS_TAR} ] ; then
-	wget -c  ${BINUTILS_URI}/${BINUTILS_TAR} -O ${TMPDIR}/${BINUTILS_TAR}
+if [ ! -f ${TMP_DIR}/${BINUTILS_TAR} ] ; then
+	wget -c  ${BINUTILS_URI}/${BINUTILS_TAR} -O ${TMP_DIR}/${BINUTILS_TAR}
 else
-	pushd "${TMPDIR}" >/dev/null
+	pushd "${TMP_DIR}" >/dev/null
 	md5sum --status -c "${BINUTILS_TAR}".md5
 	RESULT=$?
 	if [ ${RESULT} -ne 0 ] ; then
@@ -94,14 +95,14 @@ for pkg in `echo "${BINUTILS_PKGS}"` ; do
 	ver=$(dpkg-parsechangelog | grep ^"Version" | sed -r 's/Version: [0-9]{1}:([0-9\.]+)-.*/\1/')
 	popd >/dev/null
 	# Remove stale crap...
-	rm -rf "${BUILDDIR}"/"${pkg}"[_-]"${ver}"*
-	mkdir -p "${BUILDDIR}"/"${pkg}"-"${ver}"
-	cp -a "${TMPDIR}"/"${BINUTILS_TAR}" "${BUILDDIR}"/"${pkg}"-"${ver}"
-	pushd "${BUILDDIR}" >/dev/null
+	rm -rf "${BUILD_DIR}"/"${pkg}"[_-]"${ver}"*
+	mkdir -p "${BUILD_DIR}"/"${pkg}"-"${ver}"
+	cp -a "${TMP_DIR}"/"${BINUTILS_TAR}" "${BUILD_DIR}"/"${pkg}"-"${ver}"
+	pushd "${BUILD_DIR}" >/dev/null
 	tar cvfj "${pkg}"_"${ver}".orig.tar.bz2 "${pkg}"-"${ver}"
 	popd >/dev/null
-	cp -a "${pkg}"/* "${BUILDDIR}"/"${pkg}"-"${ver}"
-	pushd "${BUILDDIR}"/"${pkg}"-"${ver}" >/dev/null
+	cp -a "${pkg}"/* "${BUILD_DIR}"/"${pkg}"-"${ver}"
+	pushd "${BUILD_DIR}"/"${pkg}"-"${ver}" >/dev/null
 	build_debs 
 	popd >/dev/null
 done
@@ -110,10 +111,10 @@ return $?
 
 # Need to make GCC next
 function build_gcc {
-if [ ! -f ${TMPDIR}/${GCC_TAR} ] ; then
-	wget -c  ${GCC_URI}/${GCC_TAR} -O ${TMPDIR}/${GCC_TAR}
+if [ ! -f ${TMP_DIR}/${GCC_TAR} ] ; then
+	wget -c  ${GCC_URI}/${GCC_TAR} -O ${TMP_DIR}/${GCC_TAR}
 else
-	pushd "${TMPDIR}" >/dev/null
+	pushd "${TMP_DIR}" >/dev/null
 	md5sum --status -c "${GCC_TAR}".md5
 	RESULT=$?
 	if [ ${RESULT} -ne 0 ] ; then
@@ -121,21 +122,21 @@ else
 	fi
 	popd >/dev/null
 fi
-mkdir -p "${BUILDDIR}"
-pushd "${WORKDIR}"/"${GCC_PKG}" >/dev/null
+mkdir -p "${BUILD_DIR}"
+pushd "${WORK_DIR}"/"${GCC_PKG}" >/dev/null
 ver=$(dpkg-parsechangelog | grep ^"Version" | sed -r 's/Version: [0-9]{1}:([0-9\.].*dfsg)+.*/\1/')
 popd >/dev/null
-rm -rf "${BUILDDIR}"/"${GCC_PKG}"[_-]*
-pushd "${BUILDDIR}" >/dev/null
+rm -rf "${BUILD_DIR}"/"${GCC_PKG}"[_-]*
+pushd "${BUILD_DIR}" >/dev/null
 echo "Copying in pristine GCC source"
-tar xf "${TMPDIR}"/"${GCC_TAR}"
+tar xf "${TMP_DIR}"/"${GCC_TAR}"
 mv "${GCC_DIR}" "${GCC_PKG}"-"${ver}"
 popd >/dev/null
-cp -a "${GCC_PKG}"/* "${BUILDDIR}"/"${GCC_PKG}"-"${ver}"
-pushd "${BUILDDIR}" >/dev/null
+cp -a "${GCC_PKG}"/* "${BUILD_DIR}"/"${GCC_PKG}"-"${ver}"
+pushd "${BUILD_DIR}" >/dev/null
 echo "Making deb .orig tarball"
 tar cjf "${GCC_PKG}"_"${ver}".orig.tar.bz2 "${GCC_PKG}"-"${ver}"
-pushd "${BUILDDIR}"/"${GCC_PKG}"-"${ver}" >/dev/null
+pushd "${BUILD_DIR}"/"${GCC_PKG}"-"${ver}" >/dev/null
 build_debs 
 popd >/dev/null
 popd >/dev/null
@@ -144,15 +145,15 @@ return $?
 
 # Need to make Newlib
 function build_newlib {
-mkdir -p "${BUILDDIR}"
-pushd "${BUILDDIR}" >/dev/null
+mkdir -p "${BUILD_DIR}"
+pushd "${BUILD_DIR}" >/dev/null
 if [ ! -d "${NEWLIB_DIR}" ] ; then
 	git clone "${NEWLIB_GIT}" "${NEWLIB_DIR}"
 fi
 pushd "${NEWLIB_DIR}" >/dev/null
 git pull
 pushd src >/dev/null
-cp -a "${WORKDIR}"/"${NEWLIB_DIR}"/* ./
+cp -a "${WORK_DIR}"/"${NEWLIB_DIR}"/* ./
 build_debs
 popd >/dev/null
 popd >/dev/null
@@ -161,19 +162,19 @@ return $?
 }
 
 function build_metapkg {
-if [ ! -d "${OUTDIR}"/all ] ; then
-	mkdir "${OUTDIR}"/all
+if [ -d "${BUILD_DIR}"/"${METAPKG_DIR}" ] ; then
+	rm -rf "${BUILD_DIR}"/"${METAPKG_DIR}"
 fi
-pushd "${METAPKG_DIR}" >/dev/null
-equivs-build ns-control
-mv *.deb "${OUTDIR}"/all
+cp -a "${METAPKG_DIR}" "${BUILD_DIR}"
+pushd ""${BUILD_DIR}"/${METAPKG_DIR}" >/dev/null
+build_debs
 popd >/dev/null
 }
 
 function build_clean {
-rm  -f "${WORKDIR}"/"${BINUTILS_TAR}"
-rm -rf "${BUILDDIR}"
-rm -rf "${OUTDIR}"
+rm  -f "${WORK_DIR}"/"${BINUTILS_TAR}"
+rm -rf "${BUILD_DIR}"
+rm -rf "${OUT_DIR}"
 }
 
 function help {
